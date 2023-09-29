70acb9d6ab3140c6f7c186cb8ca80579f23ceb40
Eesha Shetty <ee.shetty@gmail.com>
2023-09-29 11:05:44 -0400

    Moving to GitHub

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,35 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Eesha Shetty on 7/22/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack () {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar{
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    }
+}
